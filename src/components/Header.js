48feb8005a6fa19c859729ea0b5270f81cffe14c
lotaxgizmo <lotaoko2@gmail.com>
2024-04-10 21:58:53 +0100

    svsdf

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,14 @@ import squidwardheader from '../assets/squidwardheader.svg'
 import squidxx from '../assets/squidxx.svg'
 import squidtelee from '../assets/squidtelee.svg'
 import squiddexx from '../assets/squiddexx.svg'
+import reddit from '../assets/reddit.svg'
 
 function Header() {
     return (
         <div className='fixed flex flex-col w-full items-center justify-center z-50 '>
             {/* <img src={squidwardheader} alt="" /> */}
             <div className="header bg-white flex items-center justify-center lg:justify-between border-4 border-[#284063] pt-5 -mt-5 rounded-3xl w-10/12 lg:px-5">
-                <div className="socials flex">
+                <div className="socials flex items-center justify-center">
 
                     <a href="https://X.com/squidoneth" target="_blank" rel="noopener noreferrer" className=' z-50 '>
                         <img src={squidxx} alt="" className='px-1' />
@@ -21,6 +22,9 @@ function Header() {
 
                     <a href="https://www.dextools.io/app/en/ether/pair-explorer/0xdfbb47a226b17044592c4e8ecc0819301f9f1e34" target="_blank" rel="noopener noreferrer" className=' z-50 '>
                         <img src={squiddexx} alt="" className='px-1' />
+                    </a>
+                    <a href="https://www.reddit.com/r/SquidOnETH/" target="_blank" rel="noopener noreferrer" className=' z-50 '>
+                        <img src={reddit} alt="" className='px-1 w-14' />
                     </a>
 
                 </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,33 +8,21 @@ import squidwardhero from '../assets/squidwardhero.svg'
 import squidwardhero2 from '../assets/squidwardhero2.svg'
 import jointelegram from '../assets/jointelegram.svg'
 import gradienthome from '../assets/gradienthome.png'
-<<<<<<< HEAD
-=======
 import CopyToClipboard from './CopyToClipboard';
->>>>>>> d762cc6 (signser)
 
 
 function Hero() {
     return (
         <div className='flex flex-col relative pt-44 items-center w-full' id='HOME'>
             <img src={gradienthome} alt="" className='absolute top-[-200px] left-[10px]  ' />
-<<<<<<< HEAD
-=======
             <div className='lg:hidden flex'>
       <CopyToClipboard text="0xa1a92f15c24ad358c3a4d0a8ba4f7db18fbfab2f" />
       </div>
->>>>>>> d762cc6 (signser)
             <div className="hero relative flex flex-col">
                 <img src={squidwardhero} alt="" className=' px-4 z-10 opacity-0 mt-10' />
                 <img src={squidwardhero} alt="" className=' px-4 z-10 absolute' />
                 <img src={squidwardhero2} alt="" className=' px-4 z-30 absolute' />
             </div>
-<<<<<<< HEAD
-            <a href="https://t.me/squidwardeth" className='z-10'>
-                <img src={jointelegram} alt="" className='mt-4 mb-20  px-4 z-10' />
-            </a>
-
-=======
       <div className='hidden lg:flex'>
       <CopyToClipboard text="0xa1a92f15c24ad358c3a4d0a8ba4f7db18fbfab2f" />
       </div>
@@ -42,7 +30,6 @@ function Hero() {
                 <img src={jointelegram} alt="" className='mt-4 mb-202  px-4 z-10' />
             </a>
              
->>>>>>> d762cc6 (signser)
 
             <div className=" flex justify-between images lg:absolute w-full dbg-red-500 mt-[-70px] lg:mt-auto">
                 <img src={heroleft} className='w-32 lg:w-auto  z-10' alt="" />
