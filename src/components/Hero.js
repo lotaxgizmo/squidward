5fd1b3f9d72408b6b7ccf6c9c04f66bf0f65cbe8
LOTAX GIZMO <130110886+lotaxgizmo@users.noreply.github.com>
2024-04-10 22:06:34 +0100

    Add files via upload

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,21 +8,28 @@ import squidwardhero from '../assets/squidwardhero.svg'
 import squidwardhero2 from '../assets/squidwardhero2.svg'
 import jointelegram from '../assets/jointelegram.svg'
 import gradienthome from '../assets/gradienthome.png'
+import CopyToClipboard from './CopyToClipboard';
 
 
 function Hero() {
     return (
         <div className='flex flex-col relative pt-44 items-center w-full' id='HOME'>
             <img src={gradienthome} alt="" className='absolute top-[-200px] left-[10px]  ' />
+            <div className='lg:hidden flex'>
+      <CopyToClipboard text="0xa1a92f15c24ad358c3a4d0a8ba4f7db18fbfab2f" />
+      </div>
             <div className="hero relative flex flex-col">
                 <img src={squidwardhero} alt="" className=' px-4 z-10 opacity-0 mt-10' />
                 <img src={squidwardhero} alt="" className=' px-4 z-10 absolute' />
                 <img src={squidwardhero2} alt="" className=' px-4 z-30 absolute' />
             </div>
+      <div className='hidden lg:flex'>
+      <CopyToClipboard text="0xa1a92f15c24ad358c3a4d0a8ba4f7db18fbfab2f" />
+      </div>
             <a href="https://t.me/squidwardeth" className='z-10'>
-                <img src={jointelegram} alt="" className='mt-4 mb-20  px-4 z-10' />
+                <img src={jointelegram} alt="" className='mt-4 mb-202  px-4 z-10' />
             </a>
-
+             
 
             <div className=" flex justify-between images lg:absolute w-full dbg-red-500 mt-[-70px] lg:mt-auto">
                 <img src={heroleft} className='w-32 lg:w-auto  z-10' alt="" />
